138c52b39d4c67c3223b8c5e7d1c8392a7e7a838
Ramiro <ramiro@okteto.com>
2025-03-19 13:35:05 -0700

    calculate the URL using the baggage header

--- serviceB/index.js
+++ serviceB/index.js
@@ -1,31 +1,36 @@
-import got from 'got';
-import express from 'express';
+import got from "got";
+import express from "express";
 const app = express();
 
 const PORT = 8080;
 
-app.get('/', function (req, res) {
-  res.send('Service B says hello!');
-})
+app.get("/", function (req, res) {
+  res.send("Service B says hello!");
+});
 
-app.get('/chain', async function(req, res) {
-  console.log('/chain request')
-  console.log("headers:")
+app.get("/chain", async function (req, res) {
+  console.log("/chain request");
+  console.log("headers:");
   for (var key in req.headers) {
     console.log(`- ${key}: ${req.headers[key]}`);
   }
-  
-  try{
-    const url = `https://servicec-${process.env.OKTETO_NAMESPACE}.${process.env.OKTETO_DOMAIN}/chain`;
-    const data = await got(url).text();
+
+  var ns = process.env.OKTETO_NAMESPACE;
+  if (req.headers["baggage.okteto-divert"]) {
+    ns = req.headers["baggage.okteto-divert"];
+  }
+
+  try {
+    const url = `https://serviceb-${ns}.${process.env.OKTETO_DOMAIN}/chain`;
+    const data = await got(url, options).text();
     const message = `Service B says hello from ${process.env.OKTETO_NAMESPACE}! <br />`;
     res.send(message + data);
-  } catch (error){
-    console.log(error)
+  } catch (error) {
+    console.log(error);
     res.status(500).send(err.message);
-  } 
+  }
 });
 
 app.listen(PORT, function () {
-  console.log('Started service-b server on %d', PORT);
-})
+  console.log("Started service-b server on %d", PORT);
+});
